d5393e6089941603a274f7cf4a470b995cbba8c8
snjtiam <iamsnjt@gmail.com>
2026-02-02 16:44:12 +0530

    fix: error boundary test

--- ZellerCodeChallenge/jest.config.js
+++ ZellerCodeChallenge/jest.config.js
@@ -4,6 +4,6 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation|@react-native-async-storage|@react-native-community|react-native-safe-area-context|react-native-screens|react-native-get-random-values|react-native-localize|aws-amplify|@aws-amplify)/)',
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@react-native-async-storage|@react-native-community|react-native-safe-area-context|react-native-screens|react-native-get-random-values|react-native-localize|aws-amplify|@aws-amplify|react-native-error-boundary)/)',
   ],
 };
